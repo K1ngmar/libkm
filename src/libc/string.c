8f7abf8c967435bf9f1dba08eda7c933a7e1dba4
Ingmar Kole <ikole@student.codam.nl>
2022-07-29 22:50:09 +0200

    feat: implemented strlcpy and strlcat

---
+++ src/libc/string.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                 _       _______  _____   _    _   __   __                  */
+/*                (_)     (_______)(_____) (_)  (_) (__)_(__)                 */
+/*                (_)        (_)   (_)__(_)(_)_(_) (_) (_) (_)                */
+/*                (_)        (_)   (_____) (____)  (_) (_) (_)                */
+/*                (_)____  __(_)__ (_)__(_)(_) (_) (_)     (_)                */
+/*                (______)(_______)(_____) (_)  (_)(_)     (_)                */
+/*                                                                            */
+/*                   Kingmar  |  https://github.com/K1ngmar                   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include <libkm.h>
+#include <string.h>
+
+size_t	km_strlcpy(char* restrict dst, const char* restrict src, size_t dstsize)
+{
+	const size_t src_len = km_strlen(src);
+
+	if (src_len < dstsize) {
+		// src fits entirely into dst
+		km_memcpy(dst, src, src_len + 1);
+	} else if (dstsize != 0) {
+		// dst is smaller than src
+		km_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
+	}
+	return src_len;
+}
+
+size_t	km_strlcat(char* restrict dst, const char* restrict src, size_t dstsize)
+{
+	const size_t src_len = km_strlen(src);
+	const size_t dst_len = km_strnlen(dst, dstsize);
+
+	if (dst_len == dstsize) {
+		// dst is completely filled
+		return dstsize + src_len;
+	}
+
+	if (src_len < dstsize - dst_len) {
+		// src will fit entirely in dst
+		km_memcpy(dst + dst_len, src, src_len + 1);
+	} else {
+		// src will partially fit into dst
+		km_memcpy(dst + dst_len, src, dstsize - dst_len - 1);
+		dst[dstsize - 1] = '\0';
+	}
+	return (dst_len + src_len);
+}
---
+++ tests/string_test.c
@@ -0,0 +1,99 @@
+#include <criterion/criterion.h>
+#include <libkm.h>
+#include <string.h>
+
+#define VERY_LONG_STRING "I AM PRETTY SURE A LONG STRING WONT MAKE A DIFFERENCE BUT JUST FOR THE SAKE OF IT WE WILL TEST IT ANYWAYS. I AM PRETTY SURE A LONG STRING WONT MAKE A DIFFERENCE BUT JUST FOR THE SAKE OF IT WE WILL TEST IT ANYWAYS. I AM PRETTY SURE A LONG STRING WONT MAKE A DIFFERENCE BUT JUST FOR THE SAKE OF IT WE WILL TEST IT ANYWAYS. I AM PRETTY SURE A LONG STRING WONT MAKE A DIFFERENCE BUT JUST FOR THE SAKE OF IT WE WILL TEST IT ANYWAYS. I AM PRETTY SURE A LONG STRING WONT MAKE A DIFFERENCE BUT JUST FOR THE SAKE OF IT WE WILL TEST IT ANYWAYS."
+
+char* km_str = NULL;
+char* og_str = NULL;
+size_t km_ret = 0;
+size_t og_ret = 0;
+
+void suitesetup(void) {
+	km_str = malloc(sizeof(char) * 1024);
+	og_str = malloc(sizeof(char) * 1024);
+	km_ret = 0;
+	og_ret = 0;
+	km_str[0] = '\0';
+	og_str[0] = '\0';
+	km_str[2] = 'a';
+	og_str[2] = 'a';
+}
+
+void suiteteardown(void) {
+	free(km_str);
+	free(og_str);
+	km_str = NULL;
+	og_str = NULL;
+}
+
+TestSuite(string_test, .init=suitesetup, .fini=suiteteardown);
+
+Test(string_test, test_km_strlen_behaviour) {
+	cr_expect(km_strlen("hi") == 2);
+	cr_expect(km_strlen("") == 0);
+	cr_expect(km_strlen("PIZZA") == 5);
+	cr_expect(km_strlen("\n \n \n") == 5);
+	cr_expect(km_strlen(VERY_LONG_STRING) == sizeof(VERY_LONG_STRING) - 1);
+}
+
+Test(string_test, test_strlcpy_no_crash) {
+	km_strlcpy(NULL, "hi", 0);
+	km_strlcpy(km_str, "test", 25);
+}
+
+Test(string_test, test_strlcpy_behaviour)
+{
+	km_ret = km_strlcpy(km_str, "test", 3);
+	og_ret = strlcpy(og_str, "test", 3);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+	
+	km_ret = km_strlcpy(km_str, "pop", 1);
+	og_ret = strlcpy(og_str, "pop", 1);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+
+	km_ret = km_strlcpy(km_str, "help", 5);
+	og_ret = strlcpy(og_str, "help", 5);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+
+	km_ret = km_strlcpy(km_str, "kaaskop", 5);
+	og_ret = strlcpy(og_str, "kaaskop", 5);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+
+	km_ret = km_strlcpy(km_str, VERY_LONG_STRING, sizeof(VERY_LONG_STRING));
+	og_ret = strlcpy(og_str, VERY_LONG_STRING, sizeof(VERY_LONG_STRING));
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+}
+
+Test(string_test, test_strlcat_behaviour)
+{	
+	km_ret = km_strlcat(km_str, "this is ", 20);
+	og_ret = strlcat(og_str, "this is ", 20);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+	
+	km_ret = km_strlcat(km_str, "an epic ", 10);
+	og_ret = strlcat(og_str, "an epic ", 10);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+
+	km_ret = km_strlcat(km_str, "test ", 0);
+	og_ret = strlcat(og_str, "test ", 0);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+
+	km_ret = km_strlcat(km_str, "strlcat implementation", 8);
+	og_ret = strlcat(og_str, "strlcat implementation", 8);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+
+	km_ret = km_strlcat(km_str, "implementation", 14);
+	og_ret = strlcat(og_str, "implementation", 14);
+	cr_expect(strcmp(km_str, og_str) == 0, "comparison");
+	cr_expect(km_ret == og_ret, "return value");
+}
