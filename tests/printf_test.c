3d45f303726afd52b6f5e6f94979d76dc4bfed18
Ingmar Kole <ikole@student.codam.nl>
2022-10-08 11:57:49 +0200

    feat: added test for field witdh

--- tests/printf_test.c
+++ tests/printf_test.c
@@ -44,7 +44,7 @@ Test(printf_test, test_no_conversion) {
 	cr_assert_str_eq(km_str, og);
 }
 
-Test(printf_test, test_decimal) {
+Test(printf_test, decimal) {
 
 	km_sprintf(&km, "d = %d", 100);
 	sprintf(og, "d = %d", 100);
@@ -71,7 +71,7 @@ Test(printf_test, test_decimal) {
 	cr_assert_str_eq(km, og);
 }
 
-Test(printf_test, test_decimal_sign) {
+Test(printf_test, decimal_sign) {
 
 	km_sprintf(&km, "%+d", 0);
 	sprintf(og, "%+d", 0);
@@ -95,5 +95,48 @@ Test(printf_test, test_decimal_sign) {
 
 	km_sprintf(&km, "%+d", -17);
 	sprintf(og, "%+d", -17);
+	cr_assert_str_eq(km, og);
+}
+
+Test(printf_test, decimal_field_width) {
+
+	km_sprintf(&km, "%20d", 0);
+	sprintf(og, "%20d", 0);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%0d", 0);
+	sprintf(og, "%0d", 0);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%1d", 1);
+	sprintf(og, "%1d", 1);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%1d", 56);
+	sprintf(og, "%1d", 56);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%1d", -6);
+	sprintf(og, "%1d", -6);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%10d", INT32_MAX);
+	sprintf(og, "%10d", INT32_MAX);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%11d", INT32_MIN);
+	sprintf(og, "%11d", INT32_MIN);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%2d", -1);
+	sprintf(og, "%2d", -1);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%3d", -17);
+	sprintf(og, "%3d", -17);
+	cr_assert_str_eq(km, og);
+	
+	km_sprintf(&km, "%2d", 17);
+	sprintf(og, "%2d", 17);
 	cr_assert_str_eq(km, og);
 }
