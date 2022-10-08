71ad70f9625e5ea19ceee207d43f21a53d4495a6
Ingmar Kole <ikole@student.codam.nl>
2022-10-08 13:07:59 +0200

    Chore: added tests for left adjust

--- tests/printf_test.c
+++ tests/printf_test.c
@@ -337,3 +337,70 @@ Test(printf_test, decimal_precision_and_width_blank)
 	sprintf(og, "% 2.6d", 17);
 	cr_assert_str_eq(km, og);
 }
+
+Test(printf_test, decimal_precision_and_width_left_adjust)
+{
+	km_sprintf(&km, "%-10.20d", 0);
+	sprintf(og, "%-10.20d", 0);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-20.10d", 0);
+	sprintf(og, "%-20.10d", 0);
+	cr_assert_str_eq(km, og);
+
+	// km_sprintf(&km, "%-.0d", 0);
+	// sprintf(og, "%-.0d", 0);
+	// cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-2.1d", 1);
+	sprintf(og, "%-2.1d", 1);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-2.3d", 56);
+	sprintf(og, "%-1.3d", 56);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-3.3d", 439);
+	sprintf(og, "%-3.3d", 439);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-4.4d", -439);
+	sprintf(og, "%-4.4d", -439);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-5.5d", 56);
+	sprintf(og, "%-5.5d", 56);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-2.2d", -6);
+	sprintf(og, "%-2.2d", -6);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-5.6d", 56);
+	sprintf(og, "%-5.6d", 56);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-2.3d", -6);
+	sprintf(og, "%-2.3d", -6);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-11.10d", INT32_MAX);
+	sprintf(og, "%-11.10d", INT32_MAX);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-12.11d", INT32_MIN);
+	sprintf(og, "%-12.11d", INT32_MIN);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-5.2d", -1);
+	sprintf(og, "%-5.2d", -1);
+	cr_assert_str_eq(km, og);
+
+	km_sprintf(&km, "%-5.9d", -17);
+	sprintf(og, "%-5.9d", -17);
+	cr_assert_str_eq(km, og);
+	
+	km_sprintf(&km, "%-2.6d", 17);
+	sprintf(og, "%-2.6d", 17);
+	cr_assert_str_eq(km, og);
+}
