b9e13a29b0249e5437c10b190c57523a591feb3d
Ingmar Kole <ikole@student.codam.nl>
2023-04-14 16:20:32 +0200

    🎆 feat: added colorcodes header

---
+++ includes/colorcodes.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                 _       _______  _____   _    _   __   __                  */
+/*                (_)     (_______)(_____) (_)  (_) (__)_(__)                 */
+/*                (_)        (_)   (_)__(_)(_)_(_) (_) (_) (_)                */
+/*                (_)        (_)   (_____) (____)  (_) (_) (_)                */
+/*                (_)____  __(_)__ (_)__(_)(_) (_) (_)     (_)                */
+/*                (______)(_______)(_____) (_)  (_)(_)     (_)                */
+/*                                                                            */
+/*                   Kingmar  |  https://github.com/K1ngmar                   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EPIC_LIBKM_COLORCODES_H
+# define EPIC_LIBKM_COLORCODES_H
+
+	# define COLOR_RESET			"\033[0m"
+	# define COLOR_RED 				"\e[38;5;160m"
+	# define COLOR_GREEN			"\e[38;5;46m"
+	# define COLOR_BLUE 			"\e[38;5;33m"
+	# define COLOR_LIGHT_BLUE 		"\e[38;5;75m"
+	# define COLOR_TOXIC_YELLOW		"\e[38;5;190m"
+	# define COLOR_ORANGE			"\e[38;5;214m"
+	# define COLOR_PURPLE			"\e[38;5;13m"
+	# define COLOR_YELLOW			"\e[38;5;220m"
+	# define COLOR_PINK				"\e[38;5;205m"
+
+#endif
