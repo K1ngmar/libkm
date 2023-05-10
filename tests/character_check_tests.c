cb054039fbd84b9b1d7860038acf06007714a3a7
Ingmar Kole <ikole@student.codam.nl>
2023-05-10 14:38:12 +0200

    fix: i am getting tired of compiling on linux

--- tests/character_check_tests.c
+++ tests/character_check_tests.c
@@ -7,7 +7,7 @@
 #define TestFunc(funcname) \
 	Test(character_checks, test_##funcname) { \
 		for (int c = -255; c < 255; c++){ \
-			cr_expect(!!funcname(c) == km_##funcname(c));} \
+			cr_expect((!!funcname(c)) == km_##funcname(c));} \
 	}
 
 TestFunc(isupper)
